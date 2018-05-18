63f47f435b0174a4db621664c27035f97dfd1a16
alichtman <aaronlichtman@gmail.com>
2018-05-18 16:41:57 -0500

    README update and file rename
    #5 #2 #1

--- README.md
+++ README.md
@@ -2,11 +2,17 @@
 
 This CLI allows you to easily fill out the columns of a CSV, prompting you with information from another cell in the row, and writing your input to a column you specify.
 
+![GIF demo](images/demo.gif)
+
 ### Usage
 
 In `cell-cycle.py`...
 
 1. Modify the path to the CSV file.
-2. Modify the columns you want to be a.) displayed and b.) edited 
+2. Modify the columns you want to be a.) displayed and b.) edited
 
-Then, run with: `$ python3 cell-cycle.py`
+Then, run with: `$ python3 cell-cycle.py`.
+
+Press `q` to exit and write to the CSV file. **Your work will not be saved unless you exit like this.**
+
+**WARNING: Keep the CSV file closed while working with it.**
---
+++ cell-cycle.py
@@ -0,0 +1,69 @@
+# alichtman
+# CSV Data Entry
+
+import sys
+import pandas as pd
+import click
+from colorama import Fore, Style
+
+def new_entry(index):
+
+	print(Fore.GREEN + Style.BRIGHT + "LINE:" + str(index) + Style.RESET_ALL)
+	print(Fore.YELLOW + Style.BRIGHT + headlines["Headline"][index] + Style.RESET_ALL)
+
+	user_input = input().lower()
+
+	# BACK
+	if user_input == 'b':
+		return (user_input, True);
+
+	# QUIT
+	elif user_input == 'q':
+		print("EXIT")
+		headlines.to_csv(path, sep=",")
+		sys.exit()
+
+	else:
+		return (user_input, False)
+
+
+def main():
+
+	path = "csv_path_here.csv"  # PLACE CSV PATH HERE
+
+	headlines = pd.read_csv(path, sep=",")
+
+	# print(headlines)
+
+	for idx in range(len(headlines["Sentiment"])):
+		# print("CURR", headlines["Sentiment"][idx])
+
+		valid = ["-2", "-1", "0", "1", "2", "x"]
+
+		if str(headlines["Sentiment"][idx]) not in valid:
+			output = new_entry(idx)
+			repeat = output[1]
+			user_input = output[0]
+
+			# print(output)
+			if repeat:
+				user_input = new_entry(idx - 1)[0]
+				if user_input in valid:
+					headlines["Sentiment"][idx-1] = user_input
+
+				user_input = new_entry(idx)[0]
+
+			if user_input in valid:
+				headlines["Sentiment"][idx] = user_input
+
+
+	headlines.to_csv(path, sep=",")
+
+
+def cli():
+	sys.exit()
+
+
+if __name__ == '__main__':
+	cli()
+
